db2d06a138ea4b65ced0a84d238ea1babc42ef3f
Mark Phillips <mphil27524@gmail.com>
2022-02-17 23:05:06 -0600

    created pokemon_type model

---
+++ db/migrate/20220218050127_create_pokemon_types.rb
@@ -0,0 +1,11 @@
+class CreatePokemonTypes < ActiveRecord::Migration[7.0]
+  def change
+    create_table :pokemon_types do |t|
+      t.integer :pokemon_type_id
+      t.integer :pokemon_id
+      t.integer :type_id
+
+      t.timestamps
+    end
+  end
+end
