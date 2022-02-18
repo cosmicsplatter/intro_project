5e1b3ba114228584b09e2ea68a861538f26c5eef
Mark Phillips <mphil27524@gmail.com>
2022-02-18 01:27:30 -0600

    added pokemon to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require "database_cleaner"
+
+PokemonType.destroy_all
+PokemonEggGroup.destroy_all
+Pokemon.destroy_all
+Type.destroy_all
+EggGroup.destroy_all
+
+DatabaseCleaner.clean_with(:truncation)
+
+require "net/http"
+require "json"
+
+pokemon_array = []
+# species_array = []
+# type_array = []
+# egg_group_array = []
+
+(1..151).each do |i|
+  pokemon_data = "https://pokeapi.co/api/v2/pokemon/#{i}"
+  uri = URI(pokemon_data)
+  response = Net::HTTP.get(uri)
+  pokemon_json = JSON.parse(response)
+  pokemon_array.push(pokemon_json)
+
+  # species = open("https://pokeapi.co/api/v2/pokemon-species/#{i}").read
+  # json_species = JSON.parse(species)
+  # species_array.push(json_species)
+end
+
+pokemon_array.each do |pokemon|
+  Pokemon.create(
+    pokemon_id: pokemon["id"],
+    number:     pokemon["id"],
+    name:       pokemon["name"],
+    sprite:     pokemon["sprites"]["front_default"]
+  )
+end
